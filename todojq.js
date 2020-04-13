40cff5e1cd68282e2ad9794a647eeac3ad7127f1
Rafeena Usman <mrs.chonasingh@gmail.com>
2020-04-12 22:48:35 -0400

    made some changes

--- todojq.js
+++ todojq.js
@@ -1,10 +1,15 @@
 //add item to list
 $('#btn').click(function getInput(){
-  var newTask = $('#input').val()
-    $('#list').append('<li class="list-group-item-info">' + newTask + '</li>');
+  var newTask = $('#input').val()//gets the value of the input
+  $('#list').append('<li>' + newTask + '</li>').addClass('list-group-item-info');// adds the new task to the ul with a class
+   // $('#list').append('<li class="list-group-item-info" id="li">' + newTask + '</li>');
+  $('#input').val("")//clears input field
 })
 
 //remove item
-$("li").click(function(){
-    $(this).remove();
-});
+$('ul').click(function garbage(event){
+    var target=event.target;//targets the item
+    if ( target.tagName.toLowerCase() === "li") {// if the target, tag name (in lowercase) = li then
+        target.remove();// remove the item
+    };
+    })
